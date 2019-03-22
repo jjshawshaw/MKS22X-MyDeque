ca18a7a55659ad2633ec4f9f9e32706391d8c525
jjshawshaw <jshaw00@stuy.edu>
2019-03-22 11:48:59 -0400

    progress on addLast function

--- Driver.java
+++ Driver.java
@@ -1,9 +1,10 @@
 public class Driver {
   public static void main(String[] args){
-    MyDeque<Integer> d = new MyDeque<Integer>(1);
-    System.out.println(d);
-    d.addFirst(1);
-    d.addFirst(1);
+    MyDeque<Integer> d = new MyDeque<Integer>(4);
+    //System.out.println(d);
+    for (int i = 1; i < 11; i++){
+      d.addLast(i);
+    }
     System.out.println(d);
   }
 }
--- MyDeque.java
+++ MyDeque.java
@@ -7,14 +7,14 @@ public class MyDeque<E>{
   public MyDeque(){
     data = (E[])new Object[10];
     start = 0;
-    end = 10;
+    end = 0;
     size = 0;
   }
 
   public MyDeque(int initialCapacity){
     data = (E[])new Object[initialCapacity];
     start = 0;
-    end = initialCapacity - 1;
+    end = 0;
     size = 0;
   }
   public int size(){
@@ -33,32 +33,53 @@ public class MyDeque<E>{
     if (start == size){
       E[] d = (E[]) new Object[data.length * 2];
       int i = 0;
-      for (int di = start; di < data.length && di <= end; di++){
+      for (int di = start; di < data.length && di < end; di++){
         d[i] = data[di];
         i++;
       }
-      for (int di = 0; di <= end && di < start; di++){
+      for (int di = 0; di < end && di < start; di++){
         d[i] = data[di];
         i++;
       }
     }
   }
   public void addLast(E element){
-    if (start == size){
+    if (size > 0) end++;
+    System.out.println(this);
+    System.out.println("adding " + element);
+    if (size == data.length){
+      System.out.println("resizing: " + data.length + " to " + data.length * 2);
       E[] d = (E[]) new Object[data.length * 2];
       int i = 0;
       for (int di = start; di < data.length && di <= end; di++){
+        System.out.println("di: " + di + " i: " + i + " num: " + data[di]
+        + "start: " + start + " end: " + end + " len: " + data.length);
         d[i] = data[di];
         i++;
       }
       for (int di = 0; di <= end && di < start; di++){
+        System.out.println(data[di]);
         d[i] = data[di];
         i++;
       }
+      start = 0;
+      end = data.length;
+      data = d;
     }
+    data[end] = element;
+    if (end == data.length) end = 0;
+    size++;
    }
   //public E removeFirst(){ }
   //public E removeLast(){ }
   //public E getFirst(){ }
   //public E getLast(){ }
+  public static void main(String[] args){
+    MyDeque<Integer> d = new MyDeque<Integer>(4);
+    //System.out.println(d);
+    for (int i = 1; i < 11; i++){
+      d.addLast(i);
+    }
+    System.out.println(d);
+  }
 }
